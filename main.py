1d2de1e9c93bc3b12ba82b9e6561010df8176133
15bubbles <15bubbles.15bubbles@gmail.com>
2024-12-26 22:55:29 +0100

    tweak parameters, add generation of annotated image

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import cv2
 from pathlib import Path
 
 
+BLUE  = (255, 0, 0)
+GREEN = (0, 255, 0)
+RED   = (0, 0, 255)
+
+
 def save_image(path: Path | str, image: cv2.typing.MatLike) -> None:
     cv2.imwrite(str(path), image)
 
@@ -15,17 +20,18 @@ def extract_stamps(filename: Path | str, output_dir: Path | str) -> None:
     save_image(output_path / "debug_grascale.jpg", image_gray)
 
     print("--- Saving binary image...")
-    _, thresh = cv2.threshold(image_gray, 60, 255, cv2.THRESH_BINARY)
+    _, thresh = cv2.threshold(image_gray, 100, 255, cv2.THRESH_BINARY)
     save_image(output_path /"debug_binary.jpg", thresh)
 
     print("--- Saving original image with contours...")
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     image_countured = image.copy()
-    cv2.drawContours(image_countured, contours, -1, (0, 255, 0), 2)
+    cv2.drawContours(image_countured, contours, -1, GREEN, 2)
     save_image(output_path / "debug_contoured.jpg", image_countured)
 
     extracted_stamp_paths = []
     counter = 0
+    image_annotated = image.copy()
 
     for contour in contours:
         area = cv2.contourArea(contour)
@@ -35,6 +41,7 @@ def extract_stamps(filename: Path | str, output_dir: Path | str) -> None:
 
         x, y, width, height = cv2.boundingRect(contour)
         image_stamp = image[y:y+height, x:x+width]
+        cv2.rectangle(image_annotated, (x, y), (x+width, y+height), GREEN, 4)
 
         counter += 1
         image_stamp_path = output_path / f"stamp_{counter}.jpg"
@@ -42,6 +49,10 @@ def extract_stamps(filename: Path | str, output_dir: Path | str) -> None:
         extracted_stamp_paths.append(image_stamp_path)
 
     print(f"--- Saved {len(extracted_stamp_paths)} extracted images")
+
+    print("--- Annotating original image with found stamps...")
+    save_image(output_path / "annotated.jpg", image_annotated)
+    print(f"--- Saved annotated image")
 
 
 if __name__ == "__main__":
