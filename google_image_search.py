3f6f05fba925541251f76b3793571bce56dc712d
15bubbles <15bubbles.15bubbles@gmail.com>
2025-01-03 09:04:15 +0100

    refactor google image search + reformat with black

--- google_image_search.py
+++ google_image_search.py
@@ -4,32 +4,54 @@ from pathlib import Path
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.remote.webdriver import WebDriver
 
 IMAGES: list[Path] = [
     Path("images_out/stamp_2.jpg").absolute(),
     Path("images_out/stamp_22.jpg").absolute(),
 ]
+
 GOOGLE_LENS_URL = "https://lens.google.com"
 
+REJECT_BUTTON_XPATH = "//span[text()='Odrzuć wszystko']"
 
-def search_image(image_path: Path) -> None:
+SEND_FILE_BUTTON_XPATH = "//span[text()='prześlij plik']"
+IMAGE_INPUT_XPATH = "//input[@name='encoded_image']"
+
+PRIZE_LABEL_CSS_SELECTOR = "span[alt='Tag etykiety produktu'] + span"
+
+
+def search_image(driver: WebDriver, image_path: Path) -> None:
     driver.get(GOOGLE_LENS_URL)
     time.sleep(1)
 
-    send_file_button = driver.find_element(By.XPATH, "//span[text()='prześlij plik']")
+    # search and click `Send file` button
+    send_file_button = driver.find_element(By.XPATH, SEND_FILE_BUTTON_XPATH)
     send_file_button.click()
     time.sleep(1)
 
-    input_element = driver.find_element(By.NAME, "encoded_image")
+    # imitate uploading a file by providing path to an image directly to form's input element
+    input_element = driver.find_element(By.XPATH, IMAGE_INPUT_XPATH)
     input_element.send_keys(str(image_path))
     time.sleep(5)
 
-    find_prize()
+    # find prizes
+    prizes = find_image_prizes(driver)
+
+    if prizes:
+        print(
+            f"Most probable prize value for stamp (that appears on top of results): {prizes[0]}"
+        )
+
+    print("No prize values found for stamp")
 
 
-def find_prize() -> str | None:
-    # Inject JavaScript to get elements based on visual order
+def find_image_prizes(driver: WebDriver) -> list[str]:
+    # inject JavaScript to get elements based on visual order
+    # because Google renders results in a Grid, which means that first element
+    # found from HTML might not exactly be the first element that has highest
+    # probability to be a direct match
+    # we are interested in element that is displayed as one of first results
     script = """
     let items = Array.from(document.querySelectorAll("span[alt='Tag etykiety produktu'] + span"));
 
@@ -42,23 +64,22 @@ def find_prize() -> str | None:
     .map((el) => el.value);
     """
     prize_values = driver.execute_script(script)
-    
-    if prize_values:
-        print(f"Returning most probable prize value for stamp: {prize_values[0]}")
-        return prize_values[0]
-    
-    print("No prize values found for stamp")
+    return prize_values
+
+
+def reject_consents(driver: WebDriver) -> None:
+    reject_button = driver.find_element(By.XPATH, REJECT_BUTTON_XPATH)
+    reject_button.click()
 
 
 if __name__ == "__main__":
     driver = webdriver.Firefox()
     driver.get(GOOGLE_LENS_URL)
-    reject_button = driver.find_element(By.XPATH, "//span[text()='Odrzuć wszystko']")
-    reject_button.click()
+    reject_consents(driver)
     time.sleep(1)
-    
+
     for image in IMAGES:
-        search_image(image)
-        time.sleep(10)
+        search_image(driver, image)
+        time.sleep(3)
 
     driver.close()
